4bf39257becb7972de821b90a02d24860c5cdc62
Matit Leesathapron <44438762+MLSTS@users.noreply.github.com>
2019-02-13 20:55:53 +0700

    Added CSS333 Homework 3.

--- ArraySum.py
+++ ArraySum.py
@@ -3,20 +3,18 @@ import multiprocessing
 
 NUM_THREADS = 4
 
-def run(id, dataA, dataB, dataC, maxValue):
+def run(id, dataA, dataB, dataC):
     chunkSize = int (len(dataC) / NUM_THREADS)
     start = chunkSize * id
     end = chunkSize * (id + 1)
     
     for i in range(start,end):
         dataC[i] = dataA[i] + dataB[i]
-    maxValue[id] = max(dataC[start:end])
 
 if __name__ == '__main__':
     dataA = multiprocessing.Array('i', 1000) # i = integer, d = double
     dataB = multiprocessing.Array('i', 1000) # i = integer, d = double
     dataC = multiprocessing.Array('i', 1000) # i = integer, d = double
-    maxValue = multiprocessing.Array('i', 4)
 
     for i in range(len(dataA)):
         dataA[i] = i
@@ -26,7 +24,7 @@ if __name__ == '__main__':
 
     procs = []
     for i in range(NUM_THREADS):
-        p = multiprocessing.Process( target=run, args=(i, dataA, dataB, dataC, maxValue))
+        p = multiprocessing.Process( target=run, args=(i, dataA, dataB, dataC))
         procs.append(p)
         p.start()
 
@@ -36,7 +34,3 @@ if __name__ == '__main__':
     print(dataA[:])
     print(dataB[:])
     print(dataC[:])
-    print("--------------------------------------------------")
-    print("Maximum value in each threads: ")
-    print(maxValue[:])
-    print("Global maximum value: %d" % (max(maxValue)))
